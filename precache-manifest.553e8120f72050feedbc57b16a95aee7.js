73843ffaad8aaffe655f8b5b6218dd51322ccbfd
aryaman-titan <aryamanbehera@gmail.com>
2020-08-18 18:47:06 +0530

    Updates

---
+++ precache-manifest.553e8120f72050feedbc57b16a95aee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b5cd6d3bf301ea1b432a407acaf66e",
+    "url": "/halemate-webportal-frontend/index.html"
+  },
+  {
+    "revision": "7021237a8f9d622142e3",
+    "url": "/halemate-webportal-frontend/static/js/2.e578477f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/halemate-webportal-frontend/static/js/2.e578477f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aade13c20aa0b966fb7b",
+    "url": "/halemate-webportal-frontend/static/js/main.088d02c4.chunk.js"
+  },
+  {
+    "revision": "1c0fea54a4951bed8964",
+    "url": "/halemate-webportal-frontend/static/js/runtime-main.79c7a29e.js"
+  }
+]);
